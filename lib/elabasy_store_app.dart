576c2ec5ba87d25ceac83794d83644f26bfc83cf
mohamedelabassy <mohamedelabasy144@gmail.com>
2025-01-14 19:33:37 +0200

    create native splash and create screen utils

--- lib/elabasy_store_app.dart
+++ lib/elabasy_store_app.dart
@@ -1,6 +1,7 @@
 import 'package:elabasy_store/core/app/connentivity_controller.dart';
 import 'package:elabasy_store/core/common/screens/no_network_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ElabasyStoreApp extends StatelessWidget {
   const ElabasyStoreApp({super.key});
@@ -11,24 +12,28 @@ class ElabasyStoreApp extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (_, value, __) {
         if (value) {
-          return MaterialApp(
-            title: 'Elabasy Store',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            builder: (context, widget) {
-              return Scaffold(
-                body: Builder(builder: (context) {
-                  ConnectivityController.instance.init();
-                  return widget!;
-                }),
-              );
-            },
-            home: Scaffold(
-              appBar: AppBar(
-                title: const Text(' Elabasy Store'),
+          return ScreenUtilInit(
+            designSize: const Size(375, 812),
+            minTextAdapt: true,
+            child: MaterialApp(
+              title: 'Elabasy Store',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              builder: (context, widget) {
+                return Scaffold(
+                  body: Builder(builder: (context) {
+                    ConnectivityController.instance.init();
+                    return widget!;
+                  }),
+                );
+              },
+              home: Scaffold(
+                appBar: AppBar(
+                  title: const Text(' Elabasy Store'),
+                ),
               ),
             ),
           );
