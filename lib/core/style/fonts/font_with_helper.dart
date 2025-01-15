82f0d4b9a3801e2544eae278a9c85c4653f12896
mohamedelabassy <mohamedelabasy144@gmail.com>
2025-01-15 16:12:44 +0200

    create font family done

---
+++ lib/core/style/fonts/font_with_helper.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class FontWithHelper {
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight regular = FontWeight.w400;
+}
