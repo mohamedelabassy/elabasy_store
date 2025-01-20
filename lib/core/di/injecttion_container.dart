7b472ec0d248ac958612460d9b29f207d22df7ed
mohamedelabassy <mohamedelabasy144@gmail.com>
2025-01-20 02:32:42 +0200

    dio factory done

--- lib/core/di/injecttion_container.dart
+++ lib/core/di/injecttion_container.dart
@@ -1,4 +1,6 @@
 import 'package:elabasy_store/core/app/cubit/app_cubit.dart';
+import 'package:elabasy_store/core/services/graphql/api_service.dart';
+import 'package:elabasy_store/core/services/graphql/dio_factory.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -7,4 +9,9 @@ Future<void> setupInjector() async {
   await _initCore();
 }
 
-Future<void> _initCore() async => sl.registerFactory(AppCubit.new);
+Future<void> _initCore() async {
+  final dio = DioFactory.getDio();
+  sl
+    ..registerFactory(AppCubit.new)
+    ..registerLazySingleton<ApiService>(() => ApiService(dio));
+}
